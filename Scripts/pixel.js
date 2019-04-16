9008ae2d0ff6a0540e9a363d277ecfd53a26e950
lblades <lblades@bresnan.net>
2019-04-16 10:35:39 -0600

    Images

---
+++ Scripts/pixel.js
@@ -0,0 +1,13 @@
+/*
+ * Auto-generated content from the Brackets New Project extension.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global $, window, document */
+
+// Simple jQuery event handler
+
+function setPixelColor(pixel)
+{
+    pixel.style.backgroundColor = 'black';
+}
