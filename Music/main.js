1e893dcffebd71cb6a5a587019f46ada58d31fa2
lblades <lblades@bresnan.net>
2019-05-07 10:01:55 -0600

    Music Boxes

---
+++ Music/main.js
@@ -0,0 +1,65 @@
+/*
+ * Auto-generated content from the Brackets New Project extension.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global $, window, document */
+
+// Simple jQuery event handler
+    
+
+$(document).ready(function () {
+var aNote = document.getElementById('aAudio');
+    $('#a').mousedown(function(){
+        aNote.currentTime =0;
+        aNote.play();
+    }
+});
+    
+$(document).ready(function () {
+var bNote = document.getElementById('bAudio');
+    $('#b').mousedown(function(){
+        bNote.currentTime =0;
+        bNote.play();
+    }
+});
+        
+$(document).ready(function () {
+var cNote = document.getElementById('cAudio');
+    $('#c').mousedown(function(){
+        cNote.currentTime =0;
+        cNote.play();
+    }
+});
+    
+    $(document).ready(function () {
+var dNote = document.getElementById('dAudio');
+    $('#d').mousedown(function(){
+        dNote.currentTime =0;
+        dNote.play();
+    }
+});
+        
+$(document).ready(function () {
+var eNote = document.getElementById('eAudio');
+    $('#e').mousedown(function(){
+        eNote.currentTime =0;
+        eNote.play();
+    }
+});
+    
+$(document).ready(function () {
+var fNote = document.getElementById('fAudio');
+    $('#f').mousedown(function(){
+        fNote.currentTime =0;
+        fNote.play();
+    }
+});
+    
+$(document).ready(function () {
+var gNote = document.getElementById('gAudio');
+    $('#g').mousedown(function(){
+        gNote.currentTime =0;
+        gNote.play();
+    }
+});    
